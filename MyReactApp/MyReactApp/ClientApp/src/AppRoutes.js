bba8bf2835d531854dbdf5ab88198185cf6c5be9
Hadi Rezaee <se.hadirezaee@gmail.com>
2022-09-05 23:22:15 +0200

    remove unused files

--- MyReactApp/MyReactApp/ClientApp/src/AppRoutes.js
+++ MyReactApp/MyReactApp/ClientApp/src/AppRoutes.js
@@ -1,21 +1,11 @@
-import { Counter } from "./components/Counter";
 import { Department } from "./components/Department";
 import { Employee } from "./components/Employee";
-import { FetchData } from "./components/FetchData";
 import { Home } from "./components/Home";
 
 const AppRoutes = [
     {
         index: true,
         element: <Home />
-    },
-    {
-        path: '/counter',
-        element: <Counter />
-    },
-    {
-        path: '/fetch-data',
-        element: <FetchData />
     },
     {
         path: '/department',
