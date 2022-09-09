07690cf24664f3b8b1458d2eaa62ca02428bcc88
Hadi Rezaee <se.hadirezaee@gmail.com>
2022-09-09 23:35:23 +0200

    add get department view plus js fetch data

--- MyReactApp/MyReactApp/ClientApp/src/components/Department.js
+++ MyReactApp/MyReactApp/ClientApp/src/components/Department.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
 export class Department extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { deps: [] }
+    }
+
+    refreshList() {
+        fetch(process.env.REACT_APP_API + 'department')
+            .then(response => response.json())
+            .then(data => { this.setState({ deps: data }); });
+    }
+
+    componentDidMount() {
+        this.refreshList();
+    }
+
+    //componentDidUpdate() {
+    //    this.refreshList();
+    //}
+
     static displayName = Department.name;
 
     render() {
+        const { deps } = this.state;
         return (
             <div className="container">
-                <h3 className="m-3 d-flex justify-content-left">
-                    this is department page
-                </h3>
+                <Table className="mt-4" striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>DepartmentId</th>
+                            <th>DepartmentName</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {deps.map(dep => 
+                            <tr key="dep.Id">
+                                <td>{dep.Id }</td>
+                                <td>{dep.Name }</td>
+                                <td>Edit / Delete</td>
+                            </tr>
+                            )}
+                    </tbody>
+                </Table>
             </div>
         );
     }
